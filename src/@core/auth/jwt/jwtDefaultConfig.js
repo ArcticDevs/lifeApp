eae36cbf9eb3de6109a68ae697fcc8c821923b00
Dhananjay Soni <arcticdevss@gmail.com>
2022-03-01 17:51:18 +0530

    base url changed

--- src/@core/auth/jwt/jwtDefaultConfig.js
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -1,10 +1,10 @@
 export default {
   // Endpoints
   // loginEndpoint: 'http://localhost/lifeapp-api/api/admin/login',
-  registerEndpoint: 'https://dev.floto.in/lifeapp-api/api/admin/register',
-  loginEndpoint: 'https://dev.floto.in/api/admin/login',
-  // loginEndpoint: 'https://gappubobo.com/api/admin/login',
-  // registerEndpoint: 'https://dev.gappubobo.com/api/admin/register',
+  // registerEndpoint: 'https://dev.floto.in/lifeapp-api/api/admin/register',
+  // loginEndpoint: 'https://dev.floto.in/api/admin/login',
+  loginEndpoint: 'https://gappubobo.com/api/admin/login',
+  registerEndpoint: 'https://gappubobo.com/api/admin/register',
   refreshEndpoint: '/jwt/refresh-token',
   logoutEndpoint: '/jwt/logout',
 
